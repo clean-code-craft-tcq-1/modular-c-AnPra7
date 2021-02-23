d062ff42ebc725c70a84312993054258da1dba0b
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-02-23 14:39:53 +0530

    Update EventCountColorCode.h

--- EventCountColorCode.h
+++ EventCountColorCode.h
@@ -1,1 +1,28 @@
+/******************************************************************************/
+/***    \file        stats.h
+***     \author      Pratik Jaiswal               
+***
+***     \brief       Header for EventCountColorCode.h
+***/       
+/*****************************************************************************/
+
+*=============================================================================
+=======                            INCLUDES                             =======
+=============================================================================*/
+#include <stdio.h>
+#include <assert.h>
+  
+ /*=============================================================================
+=======                Enums for Color Code                             =======
+=============================================================================*/
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+
 
