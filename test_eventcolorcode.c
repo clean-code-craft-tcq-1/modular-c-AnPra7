96f21234b42353b5e661908779e818f58f4cc060
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-02-23 14:46:11 +0530

    Create test_eventcolorcode.c

---
+++ test_eventcolorcode.c
@@ -0,0 +1,53 @@
+/******************************************************************************/
+/***    \file        test_eventcolorcode.c
+***     \author      Pratik Jaiswal                 
+***
+***     \brief       Test Source file 
+***/       
+/*****************************************************************************/
+
+/*=============================================================================
+=======                            INCLUDES                             =======
+=============================================================================*/
+#include "EventCountColorCode.h"
+
+
+/*---------------------------------------------------------------------------*/
+/*           Test for obtaining Pair from number                             *
+*//*-------------------------------------------------------------------------*/
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+/*---------------------------------------------------------------------------*/
+/*           Test for obtaining Pair from number                             *
+*//*-------------------------------------------------------------------------*/
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
+
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    return 0;
+}
